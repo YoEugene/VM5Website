d55cc20ac22447c9aed0934fd2b2fd1de2ee93fd
pilagod <r03725029@ntu.edu.tw>
2015-07-22 17:00:29 +0800

    lang

--- js/lang-detect.js
+++ js/lang-detect.js
@@ -1,17 +1,22 @@
 (function(){
   var lang = window.navigator.userLanguage || window.navigator.language ;
-  var pathname = window.location.pathname.slice(1).split('.')[0];
-  console.log(pathname);
-  // console.log(lang, pathname.split("_"));
+  var pathname = window.location.pathname.slice(1).split('.')[0].split('_');
+  console.log(lang);
+  // console.log(window.location.search? "true":"false");
 
-  // switch (lang.toLowerCase()) {
-  //   case "ja":
-  //     if(window.location.pathname != "/home_japanese.html")
-  //       window.location.href = "home_japanese.html";
-  //     break;
-  //   default:
-  //     if(window.location.pathname != "/home.html" && window.location.pathname != "/")
-  //       window.location.href = "home.html";
-  //     break;
-  // }
+  // console.log(pathname.length);
+  if(!window.location.search){
+    switch (lang.toLowerCase()) {
+      case "ja-jp":
+        if(pathname.length < 2 || (pathname.length > 1 && pathname[1] !== "japanese"))
+          window.location.href = "home_japanese.html";
+        break;
+      default:
+        if(pathname.length > 1)
+          window.location.href = "home.html";
+        break;
+    }
+  }
+
+
 }());
